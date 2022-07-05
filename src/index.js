016b9c32a4fab5177e0e8ce25574a61bdbb04033
1exz <alex.buchko.m@gmail.com>
2022-07-05 09:24:17 +0300

    init:init index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+    const firstArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const secondArr = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const thirdArr = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let result = resulting(number);
+
+    if (result.length === 1) {
+        return firstArr[result];
+
+    } else if (result.length === 2) {
+        if (result > 10 && result < 20) {
+            return secondArr[result[1]];
+        } else if (result[1] == 0) {
+            return thirdArr[result[0]];
+        } else {
+            return thirdArr[result[0]] + ' ' + firstArr[result[1]];
+        }
+        
+    } else if (result.length === 3) {
+        if (result[1] == 0 && result[2] == 0) {
+            return firstArr[result[0]] + ' hundred';
+        } else if (result[1] == 0 && result[2] != 0) {
+            return firstArr[result[0]] + ' hundred ' + firstArr[result[2]];     
+        } else if (result[1] != 0 && result[2] == 0) {
+            return firstArr[result[0]] + ' hundred ' + thirdArr[result[1]];        
+        } else if (result[1] == 1) {
+            return firstArr[result[0]] + ' hundred ' + secondArr[result[2]];
+        }  else if (result[1] > 1) {
+            return firstArr[result[0]] + ' hundred ' + thirdArr[result[1]] + ' ' + firstArr[result[2]];
+        }
+    }
 }
